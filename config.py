ba52050c20199dca25adccc25d66f0c976387044
bettersquirrel <86528860+bettersquirrels@users.noreply.github.com>
2022-07-22 15:09:12 +0500

    Newbie

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class Config:
+
+    MONGO_CLIENT = AsyncIOMotorClient(os.environ['DB']) 
+    DB = MONGO_CLIENT.Leyla
+    DEFAULT_GUILD_DATA = {'color': 0xa8a6f0}
+    OLD_MONGO_CLIENT = AsyncIOMotorClient(os.environ['OLD_DB'])
+    OLD_DB = OLD_MONGO_CLIENT.Seriable.main.Seriable
